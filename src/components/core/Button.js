de524675460f9f4292bcd460607c58b68a502b2e
Tran Huu Thang <huuthangit.dn@gmail.com>
2019-10-07 23:28:11 +0700

    Add common component

---
+++ src/components/core/Button.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Button} from 'react-native-elements';
+import theme from '../../styles/theme';
+
+const CustomButton = props => {
+  const {
+    onPress,
+    title,
+    primary,
+    disabled,
+    style,
+    loading,
+  } = props;
+
+  let backgroundColor = primary ? theme.colors.primary : theme.colors.lightGray;
+  let type = primary ? 'solid' : 'outline';
+
+  return (
+    <Button
+      onPress={onPress}
+      title={title}
+      type={type}
+      disabled={disabled}
+      loading={loading}
+      titleStyle={styles.title}
+      buttonStyle={[styles.button, style, { backgroundColor }]}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+    button: {
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 6,
+        borderRadius: 6
+    },
+    title: {
+      fontSize: 17,
+      fontFamily: 'SF-Pro-Text-Semibold'
+    }
+});
+
+export default CustomButton;
---
+++ src/components/core/TextField.js
@@ -0,0 +1,30 @@
+import React, { forwardRef } from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+import theme from '../../styles/theme';
+
+const TextField = forwardRef((props, ref) => {
+    return (
+        <TextInput 
+            ref={ref}
+            placeholderTextColor={theme.colors.gray}
+            style={styles.textInput}
+            {...props}
+        />
+    );
+})
+
+const styles = StyleSheet.create({
+    textInput: {
+        borderRadius: 9,
+        height: 45,
+        borderWidth: 1,
+        borderColor: theme.colors.lightGray,
+        paddingHorizontal: 16,
+        marginBottom: 20,
+        fontSize: 17,
+        fontFamily: 'SF-Pro-Text-Regular',
+        backgroundColor: theme.colors.lightGray
+    }
+});
+
+export default TextField;
