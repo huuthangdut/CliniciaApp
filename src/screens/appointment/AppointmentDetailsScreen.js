fe3949ac157b166e97fc5d4a39918c6e05ac0336
Tran Huu Thang <huuthangit.dn@gmail.com>
2019-12-14 15:06:26 +0700

    add navigate

--- src/screens/appointment/AppointmentDetailsScreen.js
+++ src/screens/appointment/AppointmentDetailsScreen.js
@@ -7,7 +7,7 @@ import {
   ScrollView,
   Linking,
   Platform,
-  ActivityIndicator
+  ActivityIndicator,
 } from 'react-native';
 import {Avatar, Icon, Rating} from 'react-native-elements';
 import {AppointmentStatus as Status} from '../../common/enums';
@@ -61,7 +61,10 @@ const AppointmentDetailsScreen = props => {
   };
 
   const onRating = () => {
-    navigation.navigate('Rating', {appointmentId: appointment.id, doctorId: appointment.doctor.id});
+    navigation.navigate('Rating', {
+      appointmentId: appointment.id,
+      doctorId: appointment.doctor.id,
+    });
   };
 
   useEffect(() => {
@@ -78,103 +81,105 @@ const AppointmentDetailsScreen = props => {
 
   return (
     <Fragment>
-      <Header navigation={navigation}/>
+      <Header navigation={navigation} />
       {appointment ? (
-          <View style={styles.container}>
-            <View style={styles.headerInfo}>
-              <View style={styles.image}>
-                <Avatar
-                  size={60}
-                  rounded
-                  source={{uri: appointment.doctor.imageProfile}}
-                />
-              </View>
-              <View style={styles.headerTextWrapper}>
+        <View style={styles.container}>
+          <View style={styles.headerInfo}>
+            <TouchableOpacity style={styles.image} onPress={() => navigation.navigate('DoctorDetails', {id: appointment.doctor.id})}>
+              <Avatar
+                size={60}
+                rounded
+                source={{uri: appointment.doctor.imageProfile}}
+              />
+            </TouchableOpacity>
+            <View style={styles.headerTextWrapper}>
+              <TouchableOpacity onPress={() => navigation.navigate('DoctorDetails', {id: appointment.doctor.id})}>
                 <Text style={styles.headerText}>{appointment.doctor.name}</Text>
-                <AppointmentStatus type={appointment.status} />
-              </View>
-              <View style={styles.contact}>
-                <TouchableOpacity
-                  style={styles.iconWrapper}
-                  onPress={() => text(appointment.doctor.phoneNumber)}>
-                  <Icon
-                    iconStyle={styles.icon}
-                    size={25}
-                    name="message-circle"
-                    type="feather"></Icon>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.iconWrapper}
-                  onPress={() => dial(appointment.doctor.phoneNumber)}>
-                  <Icon
-                    iconStyle={styles.icon}
-                    size={25}
-                    name="phone"
-                    type="font-awesome"></Icon>
-                </TouchableOpacity>
-              </View>
+              </TouchableOpacity>
+              <AppointmentStatus type={appointment.status} />
             </View>
-            <View style={styles.divider}></View>
-            <ScrollView style={styles.content}>
-              <View style={styles.itemRow}>
-                <Text style={styles.smText}>Thời gian</Text>
-                <Text style={styles.lgText}>
-                  {DateTime.toDateString(
-                    appointment.appointmentDate,
-                    'DD/MM/YYYY',
-                  )}
-                </Text>
-                <Text style={styles.smText}>
-                  {DateTime.toDateString(appointment.appointmentDate, 'HH:mm')}
-                </Text>
-              </View>
-              <View style={styles.itemRow}>
-                <Text style={styles.smText}>Địa chỉ</Text>
-                <Text style={styles.lgText}>{appointment.doctor.clinic}</Text>
-                <Text style={styles.smText}>{appointment.doctor.address}</Text>
-                {/* <Text style={styles.smText}>0.31 mi away</Text> */}
-              </View>
-              <View style={styles.itemRow}>
-                <Text style={styles.smText}>Giá khám</Text>
-                <Text style={styles.lgText}>
-                  {appointment.price ? appointment.price + 'đ' : 'Miễn phí'}
-                </Text>
-                <Text style={styles.smText}>
-                  {appointment.totalMinutes} phút
-                </Text>
-              </View>
-              <View style={styles.itemRow}>
-                <Text style={styles.smText}>Dịch vụ khám</Text>
-                <Text style={styles.lgText}>
-                  {appointment.checkingService.name}
-                </Text>
-                <Text style={styles.smText}>
-                  {appointment.checkingService.description}
-                </Text>
-              </View>
-              <View style={styles.itemRow}>
-                <Text style={styles.smText}>Nhắc nhở</Text>
-                <Text style={styles.lgText}>
-                  Trước {appointment.sendNotificationBeforeMinutes} phút
-                </Text>
-              </View>
-            </ScrollView>
-            {appointment.status === Status.Confirming.value ? (
-              <Button
-                title="Huỷ lịch hẹn"
-                primary
-                style={styles.button}
-                onPress={() => cancelAppointment(appointment.id)}
-                loading={isCancelling}
-              />
-            ) : appointment.status === Status.Confirmed.value ? (
-              <Button
-                title="Huỷ lịch hẹn"
-                secondary
-                disabled
-                style={styles.button}
-              />
-            ) : appointment.status === Status.Completed.value && !appointment.hasReview && (
+            <View style={styles.contact}>
+              <TouchableOpacity
+                style={styles.iconWrapper}
+                onPress={() => text(appointment.doctor.phoneNumber)}>
+                <Icon
+                  iconStyle={styles.icon}
+                  size={25}
+                  name="message-circle"
+                  type="feather"></Icon>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.iconWrapper}
+                onPress={() => dial(appointment.doctor.phoneNumber)}>
+                <Icon
+                  iconStyle={styles.icon}
+                  size={25}
+                  name="phone"
+                  type="font-awesome"></Icon>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.divider}></View>
+          <ScrollView style={styles.content}>
+            <View style={styles.itemRow}>
+              <Text style={styles.smText}>Thời gian</Text>
+              <Text style={styles.lgText}>
+                {DateTime.toDateString(
+                  appointment.appointmentDate,
+                  'DD/MM/YYYY',
+                )}
+              </Text>
+              <Text style={styles.smText}>
+                {DateTime.toDateString(appointment.appointmentDate, 'HH:mm')}
+              </Text>
+            </View>
+            <View style={styles.itemRow}>
+              <Text style={styles.smText}>Địa chỉ</Text>
+              <Text style={styles.lgText}>{appointment.doctor.clinic}</Text>
+              <Text style={styles.smText}>{appointment.doctor.address}</Text>
+              {/* <Text style={styles.smText}>0.31 mi away</Text> */}
+            </View>
+            <View style={styles.itemRow}>
+              <Text style={styles.smText}>Giá khám</Text>
+              <Text style={styles.lgText}>
+                {appointment.price ? appointment.price + 'đ' : 'Miễn phí'}
+              </Text>
+              <Text style={styles.smText}>{appointment.totalMinutes} phút</Text>
+            </View>
+            <View style={styles.itemRow}>
+              <Text style={styles.smText}>Dịch vụ khám</Text>
+              <Text style={styles.lgText}>
+                {appointment.checkingService.name}
+              </Text>
+              <Text style={styles.smText}>
+                {appointment.checkingService.description}
+              </Text>
+            </View>
+            <View style={styles.itemRow}>
+              <Text style={styles.smText}>Nhắc nhở</Text>
+              <Text style={styles.lgText}>
+                Trước {appointment.sendNotificationBeforeMinutes} phút
+              </Text>
+            </View>
+          </ScrollView>
+          {appointment.status === Status.Confirming.value ? (
+            <Button
+              title="Huỷ lịch hẹn"
+              primary
+              style={styles.button}
+              onPress={() => cancelAppointment(appointment.id)}
+              loading={isCancelling}
+            />
+          ) : appointment.status === Status.Confirmed.value ? (
+            <Button
+              title="Huỷ lịch hẹn"
+              secondary
+              disabled
+              style={styles.button}
+            />
+          ) : (
+            appointment.status === Status.Completed.value &&
+            !appointment.hasReview && (
               <Button
                 onPress={() => onRating()}
                 icon={{
@@ -187,8 +192,9 @@ const AppointmentDetailsScreen = props => {
                 primary
                 style={styles.button}
               />
-            )}
-          </View>
+            )
+          )}
+        </View>
       ) : (
         <ActivityIndicator
           size={30}
@@ -205,7 +211,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     backgroundColor: 'white',
     paddingHorizontal: 20,
-    marginBottom: 10
+    marginBottom: 10,
   },
   headerInfo: {
     width: '100%',
